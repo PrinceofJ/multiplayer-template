197c5a9b121d3f8a94e3edc425079f6a13fa8e7d
PrinceofJ <jackson.matsumura@gmail.com>
2025-06-07 09:50:22 -0700

    ddafs

--- GameManager.gd
+++ GameManager.gd
@@ -14,14 +14,14 @@ func _ready() -> void:
 		print("i am a host")
 
 		var peer = SteamMultiplayerPeer.new()
-		peer.create_host(0)
-		#multiplayer.multiplayer_peer = peer
+		peer.create_host(1)
+		multiplayer.multiplayer_peer = peer
 	else:
 		print("i am a client")
 		var peer = SteamMultiplayerPeer.new()
 		print(MatchSetupInfo.player_steam_ids[0])
-		var test = peer.create_client(MatchSetupInfo.player_steam_ids[0], 0) #should always be the host
-		#multiplayer.multiplayer_peer = peer
+		var test = peer.create_client(MatchSetupInfo.player_steam_ids[0], 1) #should always be the host
+		multiplayer.multiplayer_peer = peer
 
 func _on_connection_success():
 	print("yay!")
